e079789074a62bf1fbfcadad42854806d7d1fbf8
Vasilii Rogin <vasya@rogin.ru>
2025-08-22 19:35:03 +0300

    Fix export

--- main.test.js
+++ main.test.js
@@ -1,16 +1,16 @@
-const is_even = require("./build/Release/is_even");
+const isEven = require("./build/Release/is_even");
 
 describe("isEven", () => {
   test("Value is 0", () => {
-    expect(is_even.isEven(0)).toBe(true);
+    expect(isEven(0)).toBe(true);
   });
   test("Value is 1", () => {
-    expect(is_even.isEven(1)).toBe(false);
+    expect(isEven(1)).toBe(false);
   });
   test("Value is 2", () => {
-    expect(is_even.isEven(2)).toBe(true);
+    expect(isEven(2)).toBe(true);
   });
   test("Value is 3", () => {
-    expect(is_even.isEven(3)).toBe(false);
+    expect(isEven(3)).toBe(false);
   });
 });
--- src/is_even.cc
+++ src/is_even.cc
@@ -22,6 +22,7 @@ using v8::Number;
 using v8::Object;
 using v8::String;
 using v8::Value;
+using v8::Function;
 
 typedef int IsEven(int);
 
@@ -127,10 +128,25 @@ void RebuildCodeMethod(const FunctionCallbackInfo<Value> &args)
     args.GetReturnValue().Set(0);
 }
 
-void Initialize(Local<Object> exports)
+void Initialize(Local<Object> exports, Local<Object> module)
 {
-    NODE_SET_METHOD(exports, "isEven", IsEvenMethod);
-    NODE_SET_METHOD(exports, "_build_code", RebuildCodeMethod);
+    Isolate *isolate = exports->GetIsolate();
+    auto ctx = isolate->GetCurrentContext();
+
+    Local<Function> isEvenFn =
+        v8::Function::New(ctx, IsEvenMethod).ToLocalChecked();
+
+    // Attach the extra method onto the function
+    isEvenFn->Set(ctx,
+                  v8::String::NewFromUtf8(isolate, "_build_code").ToLocalChecked(),
+                  v8::Function::New(ctx, RebuildCodeMethod).ToLocalChecked())
+        .Check();
+
+    // module.exports = isEvenFn
+    module->Set(ctx,
+                v8::String::NewFromUtf8(isolate, "exports").ToLocalChecked(),
+                isEvenFn)
+        .Check();
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
